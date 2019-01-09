3d4e6086182fb9614f76c2119aa7ef0431dca02e
bruinen <max.machnik@gmail.com>
2019-01-09 23:43:56 +0100

    Offer desktop, colour icons

--- js/offer.js
+++ js/offer.js
@@ -3,15 +3,20 @@ const clickedSquare = (square) => {
     document.querySelector('.offer__container').classList.add('offer__container--folded');
     document.querySelector('.offer').classList.add('offer--folded');
     squares.forEach(square => square.classList.add('offer__square--folded'));
-    hideAllCategories(square.id);
+    disactiveUnclicked(square.id);
+    colourIcon(square.id);
 }
 squares.forEach(square => {
     square.addEventListener('click', () => clickedSquare(square))
 })
 
 const categories = document.querySelectorAll('.category');
+const images = document.querySelectorAll('.offer__draw')
 
-const hideAllCategories = (exceptId) => {
+const disactiveUnclicked = (exceptId) => {
+    images.forEach(image => {
+        image.src = image.src.replace('_colour', '');
+    })
     categories.forEach(category => {
         if(category.id !== 'cat__'+exceptId) {
             category.classList.add('category--hidden')
@@ -21,4 +26,10 @@ const hideAllCategories = (exceptId) => {
     })
 }
 
-hideAllCategories();
+const colourIcon = (categoryId) => {
+    const image = document.querySelector('#'+categoryId)
+    .querySelector('img')
+    image.src = image.src.slice(0, -4)+ '_colour.svg';
+}
+
+disactiveUnclicked();
