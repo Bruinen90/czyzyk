3962b77eaeaa211562033e6dd23601150137c0a8
bruinen <max.machnik@gmail.com>
2019-01-30 22:38:48 +0100

    Removed js from offer for SEO purposes. Refactor offer and categories according to owners suggestions

--- js/productsGrid.js
+++
@@ -1,7 +0,0 @@
-const allCells = document.querySelectorAll('.productsGrid__cell');
-const toggleDescrption = (parent) => {
-    parent.classList.toggle('productsGrid__cell--active')
-}
-allCells.forEach(cell => {
-    cell.addEventListener('click', ()=> toggleDescrption(cell))
-})
